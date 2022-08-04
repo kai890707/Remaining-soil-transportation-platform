14871e6e41eb25c50720533773316f173fb9aa64
蔡銘凱 <s18113223@stu.edu.tw>
2022-08-04 23:26:54 +0800

    收容簽名頁面，將車頭車尾圖片拍攝改為可由裝置選擇檔案與拍攝

--- app/Views/sign/containmentSign.php
+++ app/Views/sign/containmentSign.php
@@ -31,44 +31,44 @@
                 </div>
                 <div class="col-8">
                     <p class="h4 text-center">人員身分</p>
-                    <p class="h5 p-0 m-0 text-center"><?= esc($permissionName)?></p>
+                    <p class="h5 p-0 m-0 text-center"><?= esc($permissionName) ?></p>
                 </div>
             </div>
         </div>
         <div class=" col-11 bg-light shadow p-4 m-1  mx-auto shadow rounded">
             <div class="row text-break text-center">
-                 <div class="col-6 border-end ">
+                <div class="col-6 border-end ">
                     <p>文件序號</p>
                 </div>
                 <div class="col-6">
-                    <p><?php echo $pdfInfo['pdf_fileNumber']?></p>
+                    <p><?php echo $pdfInfo['pdf_fileNumber'] ?></p>
                 </div>
                 <div class="col-6 border-end ">
                     <p>建築物或拆除物名</p>
                 </div>
                 <div class="col-6">
-                    <p><?php echo $pdfInfo['pdf_buildingName']?></p>
+                    <p><?php echo $pdfInfo['pdf_buildingName'] ?></p>
                 </div>
                 <div class="col-6 border-end ">
                     <p>建(拆)造號碼</p>
                 </div>
                 <div class="col-6">
-                    <p><?php echo $pdfInfo['pdf_constructNumber']?></p>
+                    <p><?php echo $pdfInfo['pdf_constructNumber'] ?></p>
                 </div>
                 <div class="col-6 border-end ">
                     <p>建築物地點</p>
                 </div>
                 <div class="col-6">
-                    <p><?php echo $pdfInfo['pdf_buildingAddress']?></p>
+                    <p><?php echo $pdfInfo['pdf_buildingAddress'] ?></p>
                 </div>
                 <div class="col-6 border-end ">
                     <p>該聯單檔案</p>
                 </div>
                 <div class="col-6">
-                    <p><button class="btn btn-outline-success"  onclick="location.href='<?php echo base_url('document/documentTable').'/'.$pdfInfo['pdf_id']?>'" >查看</button></p>
+                    <p><button class="btn btn-outline-success" onclick="location.href='<?php echo base_url('document/documentTable') . '/' . $pdfInfo['pdf_id'] ?>'">查看</button></p>
                 </div>
             </div>
-        </div>    
+        </div>
         <form id="containment_sign_form">
             <div class=" col-11 bg-light shadow p-4 m-1  mx-auto shadow">
                 <div class="mb-3">
@@ -82,17 +82,19 @@
                     <label for="shippingContents" class="form-label">載運內容(土質)</label>
                     <input type="text" class="form-control" id="shippingContents" name="shippingContents" required>
                 </div>
-                 <div class="mb-3">
+                <div class="mb-3">
                     <label for="containmentPlaceEearthFlowNumer" class="form-label">合法收容處理場所剩餘土石方流向編號</label>
                     <input type="text" class="form-control" id="containmentPlaceEearthFlowNumer" name="containmentPlaceEearthFlowNumer" required>
                 </div>
                 <div class="input-group mb-3">
                     <label class="input-group-text" for="carFront">車頭照片</label>
-                    <input type="file" class="form-control" id="carFront" multiple="multiple" accept="image/*" capture="camera" required>
+                    <!-- <input type="file" class="form-control" id="carFront" multiple="multiple" accept="image/*" capture="camera" required> -->
+                    <input type="file" class="form-control" id="carFront"  required>
                 </div>
                 <div class="input-group mb-3">
                     <label class="input-group-text" for="carBody">車斗照片</label>
-                    <input type="file" class="form-control" id="carBody" multiple="multiple" accept="image/*" capture="camera" required>
+                    <!-- <input type="file" class="form-control" id="carBody" multiple="multiple" accept="image/*" capture="camera" required> -->
+                    - <input type="file" class="form-control" id="carBody" required>
                 </div>
                 <div class="d-grid gap-2 d-md-block">
                     <a class="btn btn-success" href="#sign">前往簽名</a>
@@ -105,7 +107,7 @@
                     </div>
                     <div class="col-12 h-50 w-100 p-2">
                         <div class="row p-0 m-0 mb-2 h-100 w-100 mx-auto">
-                            <canvas id="mycanvas" class="h-100 w-100" ></canvas>
+                            <canvas id="mycanvas" class="h-100 w-100"></canvas>
                         </div>
 
                         <!-- <div id="image"></div> -->
@@ -202,22 +204,22 @@
         // console.log('touchstart')
         // console.log(evt)
         //使用狀態鎖 鎖定
-        if(writeStatus == 0){
+        if (writeStatus == 0) {
             var touchPos = getTouchPos(canvas, evt);
             ctx.beginPath(touchPos.x, touchPos.y);
             ctx.moveTo(touchPos.x, touchPos.y);
             evt.preventDefault();
             canvas.addEventListener('touchmove', touchMove, false);
         }
-       
+
     });
 
     canvas.addEventListener('touchend', function() {
         // console.log("touchend")
-        if(writeStatus == 0 ){
+        if (writeStatus == 0) {
             canvas.removeEventListener('touchmove', touchMove, false);
         }
-        
+
     }, false);
 
 
@@ -237,157 +239,156 @@
             e.preventDefault();
             var formData = new FormData(document.getElementById('containment_sign_form'));
             // var signBase64;
-            formData.append('sign',image);
-            formData.append('pdf_id',<?php echo $pdf_id?>);
+            formData.append('sign', image);
+            formData.append('pdf_id', <?php echo $pdf_id ?>);
             Main(formData);
         })
     }, false);
 
-        function compress(
-                base64,        // 源图片
-                rate,          // 缩放比例
-                callback       // 回调
-            ) {
-                //处理缩放，转格式
-                var _img = new Image();
-                _img.src = base64;
-                _img.onload = function() {
-                    var _canvas = document.createElement("canvas");
-                    var w = this.width / rate;
-                    var h = this.height / rate;
-                    _canvas.setAttribute("width", w);
-                    _canvas.setAttribute("height", h);
-                    _canvas.getContext("2d").drawImage(this, 0, 0, w, h);
-                    var base64 = _canvas.toDataURL("image/png");
-                     _canvas.toBlob(function(blob) {
-                        if(blob.size > 750*1334){        //如果还大，继续压缩
-                            compress(base64, rate, callback);
-                        }else{
-                            callback(base64);
-                        }
-                    }, "image/png");
+    function compress(
+        base64, // 源图片
+        rate, // 缩放比例
+        callback // 回调
+    ) {
+        //处理缩放，转格式
+        var _img = new Image();
+        _img.src = base64;
+        _img.onload = function() {
+            var _canvas = document.createElement("canvas");
+            var w = this.width / rate;
+            var h = this.height / rate;
+            _canvas.setAttribute("width", w);
+            _canvas.setAttribute("height", h);
+            _canvas.getContext("2d").drawImage(this, 0, 0, w, h);
+            var base64 = _canvas.toDataURL("image/png");
+            _canvas.toBlob(function(blob) {
+                if (blob.size > 750 * 1334) { //如果还大，继续压缩
+                    compress(base64, rate, callback);
+                } else {
+                    callback(base64);
                 }
-            }
-            
-        /**
-         * 圖片轉BASE64
-         */
-        const toBase64 = file => new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = error => reject(error);
-        });
+            }, "image/png");
+        }
+    }
 
-   
-        async function Main(formData) {
+    /**
+     * 圖片轉BASE64
+     */
+    const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
 
-            const carFront = document.querySelector('#carFront').files[0];
-            const carBody = document.querySelector('#carBody').files[0];
-            
-            let carFrontBase64  = await compressImgSync(document.querySelector('#carFront'));
-            let carBodyBase64  = await compressImgSync(document.querySelector('#carBody'));
 
-            for (var pair of formData.entries()) {
-                console.log(pair[0]+ ', ' + pair[1]); 
-            }
-            formData.append('carFront',carFrontBase64);
-            formData.append('carBody',carBodyBase64);
+    async function Main(formData) {
 
-            Swal.fire({
-                title: '已確認簽名了嗎?',
-                showDenyButton: true,
-                confirmButtonText: '是! 已簽名',
-                denyButtonText: `尚未簽名`,
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    BaseLib.Post("/pdf/uploadSign",formData).then(
-                    (res)=>{
-                        BaseLib.ResponseCheck(res).then(()=>{
+        const carFront = document.querySelector('#carFront').files[0];
+        const carBody = document.querySelector('#carBody').files[0];
+
+        let carFrontBase64 = await compressImgSync(document.querySelector('#carFront'));
+        let carBodyBase64 = await compressImgSync(document.querySelector('#carBody'));
+
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + ', ' + pair[1]);
+        }
+        formData.append('carFront', carFrontBase64);
+        formData.append('carBody', carBodyBase64);
+
+        Swal.fire({
+            title: '已確認簽名了嗎?',
+            showDenyButton: true,
+            confirmButtonText: '是! 已簽名',
+            denyButtonText: `尚未簽名`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                BaseLib.Post("/pdf/uploadSign", formData).then(
+                    (res) => {
+                        BaseLib.ResponseCheck(res).then(() => {
                             console.log(res);
-                            if(res.status =="success"){
-                                window.location=BaseLib.base_Url+'/project/projectList';
+                            if (res.status == "success") {
+                                window.location = BaseLib.base_Url + '/project/projectList';
                                 // window.history.go(-2);
                             }
                         })
                     },
-                    (err)=>{
+                    (err) => {
                         console.log(err);
                     })
-                } else if (result.isDenied) {
-                    Swal.fire('資料尚未更新', '', 'info')
-                }
-                
-            })
-        }
-        const compressImgSync = (imgfile) => {
-            return new Promise((resolve, reject) => {
-                let img = new Image()
-                let cvs = document.createElement('canvas')
-                let file = imgfile.files[0]
-                // 上傳圖片大於100KB就壓縮
-                if (file && file.size / 1024 > 100) {
-                    let reader = new FileReader()
-                    reader.readAsDataURL(file) // 轉成base64
-                        reader.onload = function (e) {
-                            let naturalBase64 = e.target.result // 原圖base64
-                            img.src = naturalBase64
-                            img.onload = function () {
-                            let ratio = img.naturalWidth / img.naturalHeight // 以原圖長寬比取得壓縮比
-                            cvs.width = 400 // 設定canvas寬度
-                            cvs.height = cvs.width / ratio // 根據原圖寬度比例取得高度
-                            let ctx = cvs.getContext('2d')
-                            ctx.drawImage(img, 0, 0, cvs.width, cvs.height) // 渲染至canvas
-                            // 經壓縮後新圖
-                            let zipBase64 = cvs.toDataURL("image/png")
-                                if (zipBase64) {
-                                    let urlBase64 = zipBase64.split(',')[1]
-                                    console.log('壓縮中')
-                                    resolve(urlBase64)
-                                }
-                            }
-                        }
-                } else { // 上傳圖片小於100KB以下，取原文件base64
-                    console.log('非壓縮')
-                    // let urlBase64 = imgfile.content.split(',')[1]
-                    // resolve(urlBase64)
-                    let reader = new FileReader();
-                    reader.readAsDataURL(file); // 轉成base64
-                    reader.onload = function (e) {
-                        let naturalBase64 = e.target.result 
-                        img.src = naturalBase64
-                        img.onload = function () {
-                            let ratio = img.naturalWidth / img.naturalHeight 
-                            cvs.width = 400 
-                            cvs.height = cvs.width / ratio 
-                            let ctx = cvs.getContext('2d')
-                            ctx.drawImage(img, 0, 0, cvs.width, cvs.height) 
-                            let zipBase64 = cvs.toDataURL("image/png")
-                            resolve(zipBase64)
+            } else if (result.isDenied) {
+                Swal.fire('資料尚未更新', '', 'info')
+            }
+
+        })
+    }
+    const compressImgSync = (imgfile) => {
+        return new Promise((resolve, reject) => {
+            let img = new Image()
+            let cvs = document.createElement('canvas')
+            let file = imgfile.files[0]
+            // 上傳圖片大於100KB就壓縮
+            if (file && file.size / 1024 > 100) {
+                let reader = new FileReader()
+                reader.readAsDataURL(file) // 轉成base64
+                reader.onload = function(e) {
+                    let naturalBase64 = e.target.result // 原圖base64
+                    img.src = naturalBase64
+                    img.onload = function() {
+                        let ratio = img.naturalWidth / img.naturalHeight // 以原圖長寬比取得壓縮比
+                        cvs.width = 400 // 設定canvas寬度
+                        cvs.height = cvs.width / ratio // 根據原圖寬度比例取得高度
+                        let ctx = cvs.getContext('2d')
+                        ctx.drawImage(img, 0, 0, cvs.width, cvs.height) // 渲染至canvas
+                        // 經壓縮後新圖
+                        let zipBase64 = cvs.toDataURL("image/png")
+                        if (zipBase64) {
+                            let urlBase64 = zipBase64.split(',')[1]
+                            console.log('壓縮中')
+                            resolve(urlBase64)
                         }
                     }
                 }
-            }).then(res => {
-                return res
-            }).catch(res => {
-            })
-        }
- 
-        async function jpeg2png(base64jpeg) {
-            return new Promise(resolve => {
-                const img = new Image();
-                img.setAttribute("src", base64jpeg);
-                img.setAttribute("crossOrigin", "anonymous");
-                img.onload = () => {
-                    const canvas = document.createElement("canvas");
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    const ctx = canvas.getContext("2d");
-                    ctx.drawImage(img, 0, 0);
-                    const dataURL = canvas.toDataURL("image/png");
-                    resolve(dataURL);
-                };
-        })};
- 
+            } else { // 上傳圖片小於100KB以下，取原文件base64
+                console.log('非壓縮')
+                // let urlBase64 = imgfile.content.split(',')[1]
+                // resolve(urlBase64)
+                let reader = new FileReader();
+                reader.readAsDataURL(file); // 轉成base64
+                reader.onload = function(e) {
+                    let naturalBase64 = e.target.result
+                    img.src = naturalBase64
+                    img.onload = function() {
+                        let ratio = img.naturalWidth / img.naturalHeight
+                        cvs.width = 400
+                        cvs.height = cvs.width / ratio
+                        let ctx = cvs.getContext('2d')
+                        ctx.drawImage(img, 0, 0, cvs.width, cvs.height)
+                        let zipBase64 = cvs.toDataURL("image/png")
+                        resolve(zipBase64)
+                    }
+                }
+            }
+        }).then(res => {
+            return res
+        }).catch(res => {})
+    }
+
+    async function jpeg2png(base64jpeg) {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.setAttribute("src", base64jpeg);
+            img.setAttribute("crossOrigin", "anonymous");
+            img.onload = () => {
+                const canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                const ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+                const dataURL = canvas.toDataURL("image/png");
+                resolve(dataURL);
+            };
+        })
+    };
 </script>
 <?= $this->endSection() ?>
